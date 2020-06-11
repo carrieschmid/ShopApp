8a55ca5498aba446d8d53b2c41f41b986bcef0b0
Carrie Schmid <carriepederson7@gmail.com>
2020-06-11 16:31:36 -0700

    Style order screen

--- screens/shop/OrdersScreen.js
+++ screens/shop/OrdersScreen.js
@@ -4,6 +4,7 @@ import { useSelector } from "react-redux";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 
 import HeaderButton from "../../components/UI/HeaderButton";
+import OrderItem from "../../components/shop/OrderItem";
 
 const OrdersScreen = (props) => {
   const orders = useSelector((state) => state.orders.orders);
@@ -12,7 +13,15 @@ const OrdersScreen = (props) => {
     <FlatList
       data={orders}
       keyExtractor={(item) => item.id}
-      renderItem={(itemData) => <Text>{itemData.item.totalAmount}</Text>}
+      renderItem={(itemData) => (
+        <OrderItem
+          // amount={itemData.item.totalamount}
+          //this is not working
+          date={itemData.item.readableDate}
+          //this refers to getter property
+          items={itemData.item.items}
+        />
+      )}
     />
   );
 };
